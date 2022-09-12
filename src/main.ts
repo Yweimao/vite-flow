0cbe1f374499b7934d2c4437e1a5b4f0e9879bb4
yeweimao <yeweimao@zhuanzhuan.com>
2022-09-12 16:22:48 +0800

    feat: 工程配置完成

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@
  * @Author: yeweimao yeweimao@zhuanzhuan.com
  * @Date: 2022-09-12 11:11:17
  * @LastEditors: yeweimao yeweimao@zhuanzhuan.com
- * @LastEditTime: 2022-09-12 16:12:07
+ * @LastEditTime: 2022-09-12 16:21:28
  * @FilePath: /vite-flow/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createApp } from "vue"
 import App from "./App.vue"
 createApp(App).mount("#app")
-const a: number = "123"
