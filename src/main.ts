30142c14aa1525fd3c462353fe255d2df5e8dd41
yeweimao <yeweimao@zhuanzhuan.com>
2022-09-12 16:12:27 +0800

    test: 校验自动修复功能

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@
  * @Author: yeweimao yeweimao@zhuanzhuan.com
  * @Date: 2022-09-12 11:11:17
  * @LastEditors: yeweimao yeweimao@zhuanzhuan.com
- * @LastEditTime: 2022-09-12 15:50:02
+ * @LastEditTime: 2022-09-12 16:12:07
  * @FilePath: /vite-flow/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createApp } from "vue"
 import App from "./App.vue"
 createApp(App).mount("#app")
-const a: number = "123"
+const a: string = "123"
