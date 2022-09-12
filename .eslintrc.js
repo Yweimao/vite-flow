f00010694a2266c32edecfdab5440cb58365d03c
yeweimao <yeweimao@zhuanzhuan.com>
2022-09-12 16:11:14 +0800

    feat: husky校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,27 @@
  * @Author: yeweimao yeweimao@zhuanzhuan.com
  * @Date: 2022-09-12 12:16:10
  * @LastEditors: yeweimao yeweimao@zhuanzhuan.com
- * @LastEditTime: 2022-09-12 15:37:43
+ * @LastEditTime: 2022-09-12 15:56:12
  * @FilePath: /vite-flow/.eslintrc.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 module.exports = {
   env: {
-    //针对哪些环境的语法
+    // 环境 针对那些环境的语法
     browser: true,
     es2021: true,
     node: true
   },
   extends: [
-    //集成了哪些规则, 别人写好的, 直接拿来用
+    // 集成了哪些规则， 别人写好的. 别人写好的规则拿来用
     "eslint:recommended",
-    "plugin:vue/vue3-essential",
-    "plugin:@typescript-eslint/recommended", //ts规则
+    "plugin:vue/vue3-essential", // eslint-plugin-vue
+    "plugin:@typescript-eslint/recommended", // typescript 规则
     "@vue/prettier"
   ],
   overrides: [],
-  // 可以解析.vue文件
-  parser: "@typescript-eslint/parser",
+  // 可以解析.vue 文件
+  parser: "vue-eslint-parser", // esprima babel-eslint @typescript-eslint/parser
   parserOptions: {
     parser: "@typescript-eslint/parser", // 解析ts文件的
     ecmaVersion: "latest",
@@ -30,6 +30,7 @@ module.exports = {
   },
   plugins: ["vue", "@typescript-eslint"],
   rules: {
+    "vue/multi-word-component-names": "off",
     "prettier/prettier": [
       // 自带的prettier规则
       "error",
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,11 @@
 /*
  * @Author: yeweimao yeweimao@zhuanzhuan.com
- * @Date: 2022-09-12 11:11:17
+ * @Date: 2022-09-12 16:08:13
  * @LastEditors: yeweimao yeweimao@zhuanzhuan.com
- * @LastEditTime: 2022-09-12 15:50:02
- * @FilePath: /vite-flow/src/main.ts
+ * @LastEditTime: 2022-09-12 16:08:23
+ * @FilePath: /vite-flow/commitlint.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { createApp } from "vue"
-import App from "./App.vue"
-createApp(App).mount("#app")
-const a: number = "123"
+module.exports = {
+  extends: ["@commitlint/config-conventional"]
+}
